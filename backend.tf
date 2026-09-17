24e0dd678334bb6010eecc3c28533d7ad94a4361
echeck246 <emmanueloniyide438@gmail.com>
2026-09-17 21:15:04 +0100

    fix: Updated s3 bucket to personal

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "ts-academy-state-file"
+    bucket       = "ts-academy-state-file-2026"
     key          = "tf-academy.tfstate"
     region       = "us-east-1"
   }
